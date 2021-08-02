8dcf95f8d28933944106ad7cdca717df71990086
Luis Diego <aguilarluisdi@hotmail.com>
2021-08-02 10:42:14 -0600

    read and delete test database

---
+++ relational/read_solutiondesigns.sql
@@ -0,0 +1,13 @@
+USE solutiondesigns
+
+SELECT * FROM sd_solutionslog
+SELECT * FROM sd_solutions
+SELECT * FROM sd_designs
+SELECT * FROM sd_problemlinks
+SELECT * FROM sd_problemkeywords
+SELECT * FROM sd_problems
+SELECT * FROM sd_keywords
+SELECT * FROM sd_links
+SELECT * FROM sd_owners
+SELECT * FROM sd_actiontypes
+GO
